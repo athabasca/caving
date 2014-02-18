e7175a16137ee842b6eaf03469a3d1f9895d0de3
A. Witschi <athabasca.witschi@gmail.com>
2014-02-18 00:31:09 -0800

    Finished testing move_caver, cleaned up file, added success/failure messages.

--- lost_caver.py
+++ lost_caver.py
@@ -1,19 +1,25 @@
 #! usr/bin/python
 
-# 20 wide x 16 high cave, bottom left 0,0
-# input: (x, y) <heading> <string of instructions>
-# x, y integers, heading one of {N, E, S, W},
-# each char of instructions one of {M, L, R}
-# Denotes initial position/heading followed by directions.
-# output: after each instruction is processed, print coordinates and heading.
+# lost_caver.py
+# Author: Athabasca Witschi
+# Date: February, 2014
+# 
+# About: The goal is to guide a caver to safety by sending him
+# his position and heading and then directions to the exit of the cave.
 #
-# Things to note: falling off = death. i.e. 0 <= x < 20, 0 <= y < 16
-# Can assume instructions will lead to exit of cave.
+# 20 wide x 16 high cave, bottom left is at (0,0)
+# Input: (x, y) <heading> <string of instructions>
+# x, y are integers, heading is one of {N, E, S, W},
+# each character of instructions  is one of {M, L, R}.
+# Output: after each instruction is processed, coordinates
+# and heading are printed. If you go off the edge, a death
+# message is printed. Otherwise, once there is no more input,
+# you will get a success message.
 
-from sys import stdin # Assuming input is text stream from stdin.
+from sys import stdin
 import re
 
-debug = True
+debug = False
 
 # List that maps headings to ints (the indices)
 headings = ['N', 'E', 'S', 'W']
@@ -42,10 +48,14 @@ def get_initial_position(line):
         raise ValueError(msg)
         
     return (x, y, heading)
+
     
 def move_caver(instruction, x, y, heading):
-    """ Get caver's new location given an instruction. """
+    """ Get caver's new location given an instruction, print position. """
     
+    if type(instruction) is not str:
+        raise ValueError("Invalid instruction: {0}\n".format(instruction))
+        
     if instruction == 'M': # Move 1 in faced direction.
         if heading == 0:
             y += 1
@@ -55,7 +65,7 @@ def move_caver(instruction, x, y, heading):
             y -= 1
         elif heading == 3:
             x -= 1
-        # Check if caver fell off the edge.
+        # Check if caver fell off the edge. Fatal error. Ahaha.
         if x < 0 or x >= 20 or y < 0 or y >= 16:
             print "AAAAAaaa...."
             print "This is the sound the caver makes",
@@ -70,15 +80,17 @@ def move_caver(instruction, x, y, heading):
         heading += 1
         if heading == 4:
             heading = 0
+    elif instruction.isspace():
+        return (x, y, heading)
     else:
-        print "Invalid instruction: {0}".format(instruction)
+        raise ValueError("Invalid instruction: {0}\n".format(instruction))
     
     print "({0}, {1}) {2}".format(x, y, headings[heading])    
     return (x, y, heading)
 
     
 def main():
-    """ docstring """
+    """ Guide a caver through a cave to the exit (or his death). """
     
     # Assuming initial coordinates and heading are on one line.
             
@@ -100,6 +112,15 @@ def main():
     for instruction in instructions:
         x, y, heading = move_caver(instruction, x, y, heading)
         
-	
+    # Any subsequent lines from stdin should just be instructions.
+    instruction = stdin.read(1)
+    while (instruction != ""): # while not EOF
+        x, y, heading = move_caver(instruction, x, y, heading)
+        instruction = stdin.read(1)
+        
+    print "The caver made it to the exit at ({0}, {1})!".format(x, y)
+    exit(0)
+        
+        	
 if __name__ == '__main__':
     main()
--- test_lost_caver.py
+++ test_lost_caver.py
@@ -2,42 +2,68 @@
 
 import unittest
 import lost_caver
+from string import whitespace
 
 class TestLostCaver(unittest.TestCase):
 
     def test_initial_position_regex(self):
         # Should handle different whitespace, all headings.
-        pass_cases = ["(1,2) N", "(1, 2) N", "(1,2)N", "(7, 12)W",
-                        "(12, 12) E", "(2,2) S", "(9, 11)  W"]
+        pass_cases = [
+            "(1,2) N", "(1, 2) N", "(1,2)N", "(7, 12)W",
+            "(12, 12) E", "(2,2) S", "(9, 11)  W"
+            ]
         for case in pass_cases:
             lost_caver.get_initial_position(case)
             
         # Should fail if malformed, negative ints, floats, bad headings.
-        fail_cases = ["(1,2 N", "1, 2)E", "1,2 S", "1 2 E", "(-5, 7) S",
-                        "(-5, -5) W", "(12, -6) E", "(2.3, 3) E",
-                        "dgfgfd", "(a, b) N", "(2, 6) Q"]
+        fail_cases = [
+            "(1,2 N", "1, 2)E", "1,2 S", "1 2 E", "(-5, 7) S",
+            "(-5, -5) W", "(12, -6) E", "(2.3, 3) E",
+            "dgfgfd", "(a, b) N", "(2, 6) Q"
+            ]
         for case in fail_cases:
             self.assertRaises(ValueError, lost_caver.get_initial_position, case)
             
     def test_coordinate_bounds(self):
         # 0<=x<20, 0<=y<16. Test internal and boundary values.
-        pass_cases = ["(0, 0) N", "(0, 7) N", "(8, 0) N", "(5, 9) N",
-                        "(5, 15) N", "(19, 8) N", "(19, 15) N"]
+        pass_cases = [
+            "(0, 0) N", "(0, 7) N", "(8, 0) N", "(5, 9) N",
+            "(5, 15) N", "(19, 8) N", "(19, 15) N"
+            ]
         for case in pass_cases:
             lost_caver.get_initial_position(case)
             
-        fail_cases = ["(20, 0) N", "(0, 16) N", "(20, 16) N", "(233, 43) N",
-                        "(-5, 0) N", "(0, -7) N", "(-5, -9) N"]
+        fail_cases = [
+            "(20, 0) N", "(0, 16) N", "(20, 16) N", "(233, 43) N",
+            "(-5, 0) N", "(0, -7) N", "(-5, -9) N"
+            ]
         for case in fail_cases:
             self.assertRaises(ValueError, lost_caver.get_initial_position, case)
             
     def test_move_caver(self):
         # move_caver(instruction, x, y, heading), heading as int 0-3
         # 0 - N, 1 - E, 2 - S, 3 - W
-        exit_cases = [('M', 5, 15, 0), ('M', 5, 0, 2),
-                        ('M', 19, 15, 1), ('M', 0, 15, 3)]
+        exit_cases = [
+            ('M', 5, 15, 0), ('M', 5, 0, 2),
+            ('M', 19, 15, 1), ('M', 0, 15, 3)
+            ]
         for case in exit_cases:
             self.assertRaises(SystemExit, lost_caver.move_caver, *case)
+            
+        turn_cases = [ # lists of parameter- and return-value-tuples
+            [('L', 5, 15, 0), (5, 15, 3)], [('L', 5, 15, 3), (5, 15, 2)],
+            [('L', 5, 15, 2), (5, 15, 1)], [('L', 5, 15, 1), (5, 15, 0)],
+            [('R', 5, 15, 3), (5, 15, 0)], [('R', 5, 15, 2), (5, 15, 3)]
+            ]
+        for case in turn_cases:
+            self.assertEqual(lost_caver.move_caver(*case[0]), case[1])
+            
+        for char in whitespace: # test that whitespace is ignored
+            self.assertEqual(lost_caver.move_caver(char, 5, 5, 0), (5, 5, 0))
+            
+        bad_instructions = ['Q', 7, ":", "RUN"]
+        for inst in bad_instructions:
+            self.assertRaises(ValueError, lost_caver.move_caver, *(inst, 5, 5, 0))
             
 if __name__ == '__main__':
     unittest.main()
